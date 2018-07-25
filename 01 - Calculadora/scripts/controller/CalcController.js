d403c4321a3a5bd49809da062f9d2edb9496e547
gianveloxBR <gianveloxsi@gmail.com>
2018-07-25 18:30:22 -0300

    Aula C17

--- 01 - Calculadora/scripts/controller/CalcController.js
+++ 01 - Calculadora/scripts/controller/CalcController.js
@@ -15,6 +15,7 @@ class CalcController{
     this._currentDate;
     this.initialize();
     this.initButtonsEvents();
+    this.initKeyboard();
     // Underline - Privado, Não deve ser chamado diretamente
   }
 
@@ -36,6 +37,51 @@ class CalcController{
     addEventListenerAll(element, events, fn){
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
+        });
+    }
+
+    initKeyboard(){
+        document.addEventListener('keyup', e=>{
+            switch(e.key){
+                case 'Escape':
+                this.clearAll();
+                break;
+    
+                case 'Backspace':
+                this.clearEntry();
+                break;
+    
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                this.addOperation(e.key);
+                break;
+    
+                case 'Enter':
+                case '=':
+                this.calc();
+                break;
+    
+                case '.':
+                case ',':
+                this.addDot();
+                break;
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                this.addOperation(parseInt(e.key));
+                break;
+            }
         });
     }
 
