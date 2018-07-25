08708b1796c30bc39ad81fbeefe8b6493ce7f816
gianveloxBR <gianveloxsi@gmail.com>
2018-07-25 19:14:04 -0300

    Aula C21

--- 01 - Calculadora/scripts/controller/CalcController.js
+++ 01 - Calculadora/scripts/controller/CalcController.js
@@ -159,7 +159,14 @@ class CalcController{
     }
 
     getResult(){        
+        try {
         return eval(this._operation.join(""));
+        }catch(e){
+            setTimeout(()=>{
+                this.setError();
+            }, 1);
+            
+        }
     }
 
     calc(){
