07edb7b6522cbc63bea8526954f0f7d1ab01e561
gianveloxBR <gianveloxsi@gmail.com>
2018-07-25 18:06:31 -0300

    Aula C15

--- 01 - Calculadora/scripts/controller/CalcController.js
+++ 01 - Calculadora/scripts/controller/CalcController.js
@@ -41,6 +41,8 @@ class CalcController{
 
     clearAll(){
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
         this.setLastNumberToDisplay();
     }
 
@@ -135,8 +137,6 @@ class CalcController{
             if(this.isOperator(value)){
                 //Trocar o Operador
                 this.setLastOperation(value); //Igual ao operador do momento
-            }else if(isNaN(value)){               
-               console.log('Outra Coisa',value); 
             }else{
                 this.pushOperation(value);
 
@@ -151,7 +151,7 @@ class CalcController{
 
             }else{
             let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
+            this.setLastOperation(parseFloat(newValue));
 
             //Update Display
             this.setLastNumberToDisplay();
@@ -161,6 +161,16 @@ class CalcController{
 
     setError(){
         this.displayCalc = "Error";
+    }
+
+    addDot(){
+        let lastOperation = this.getLastOperation();
+        if(this.isOperator(lastOperation) || !lastOperation){
+            this.pushOperation('0.');
+        } else{
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+        this.setLastNumberToDisplay();
     }
 
     execBtn(value){
@@ -198,7 +208,7 @@ class CalcController{
             break;
 
             case 'ponto':
-            this.addOperation('.');
+            this.addDot();
             break;
 
             case '0':
