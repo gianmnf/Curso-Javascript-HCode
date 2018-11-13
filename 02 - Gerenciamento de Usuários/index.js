7b5ee7b7840e0209e1d5d57eabb3a1ac5525e7be
Gian Michel <gianveloxsi@gmail.com>
2018-11-13 19:35:23 -0300

    Aula G3

--- 02 - Gerenciamento de Usuários/index.js
+++ 02 - Gerenciamento de Usuários/index.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+//Aula G3
+// Variável - Evita que haja repetição de código - referência de memória - não pode ser var
+// melhor usar o objeto ao invés do value - referência
+var nome = document.querySelector("#exampleInputName");
+// verifica se está ok
+//console.log(nome);
+nome.value = "Gian";
+nome.style.color = "blue";
